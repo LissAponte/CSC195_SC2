b7aeff98cc1886e236e259392751354967248dc7
Liss <japonterivera@student.neumont.edu>
2025-05-11 16:44:22 -0600

    Update MadLib.cpp

--- MadLib.cpp
+++ MadLib.cpp
@@ -11,8 +11,9 @@ void MadLib::Write() {
 	std::getline(std::cin, adjective);
 	std::cout << "Enter a number: ";
 	std::cin >> number;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //asked Copilot for help 
 	std::cout << "Enter a plural noun: ";
-	std::getline(std::cin, pluralNoun);
+	std::getline(std::cin, pluralNoun); 
 	std::cout << "Enter an adverb: ";
 	std::getline(std::cin, adverb);
 	std::cout << "Enter another verb: ";
@@ -24,7 +25,7 @@ void MadLib::Read() {
 	
 
 	std::string story = name + " the hamster is a small rodent. He always " + verb + " when the owner lets him out of the cage";
-	std::string s = " The " + adjective + " mammal has the stomach of " +  std::to_string(number) + " " + pluralNoun;
+	std::string s = " The " + adjective + " mammal has the stomach of " +  std::to_string(number) + " " + pluralNoun; 
 	std::string s1 = " Finally he " + adverb + " sleeps after a long day of " + verb2 + " around.";
 
 	std::cout << story << std::endl;
