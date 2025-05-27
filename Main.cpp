5fffcfae8cba07c43b6f8635a0934249b210a88b
Liss <japonterivera@student.neumont.edu>
2025-05-27 13:26:50 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main() {
     
-    FruityDatabase fruitydatabase;
 
+    FruityDatabase fruitydatabase;
 
 	bool quit = false;
 	while (!quit) {
         cout << "1 - Create\n2 - Display All\n3 - Display by Name\n4 - Display by Type\n5 - Quit\n";
         int choice;
         cin >> choice;
+
         switch (choice)
         {
         case 1: fruitydatabase.Create(Fruit::eType::BERRY); break;
@@ -19,8 +20,10 @@ int main() {
         case 3: 
         {
             cout << "Enter name: ";
+			cin.ignore(); // clear the newline character left in the input buffer
             string name;
             cin >> name;
+            getline(cin, name);
             fruitydatabase.Display(name); break;
         }
         case 4:
@@ -28,10 +31,21 @@ int main() {
             cout << "Enter type (0 for TYPE1, 1 for TYPE2): ";
             int t;
             cin >> t;
-            fruitydatabase.Display(static_cast<Fruit::eType>(t)); break;
+            if (cin.fail() || (t < 0 || t > 1)) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid type. Enter a number between 1 and 5.\n";
+                break;
+            }
+            fruitydatabase.Display(static_cast<Fruit::eType>(t));
+            break;
         }
         case 5:
             quit = true;
+            break;
+
+        default:
+            cout << "Invalid choice.\n";
             break;
         }
     }
