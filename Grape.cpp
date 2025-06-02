74f6b34059d297d1757463bce35f1501374a6374
Liss <japonterivera@student.neumont.edu>
2025-06-01 18:25:18 -0600

    Update Grape.cpp

--- Grape.cpp
+++ Grape.cpp
@@ -2,11 +2,13 @@
 
 void Grape::Read(std::ostream& ostream, std::istream& istream)
 {
-	ostream << "Enter grape type: ";
-	istream >> m_grape_type;
+	ostream << "Enter grape color: ";
+	istream >> m_grape_name;
+	ostream << "How many grapes?: ";
 }
 
 void Grape::Write(std::ostream& ostream)
 {
-	ostream << m_grape_type << endl;
+	ostream << m_grape_name << endl;
+	ostream << "Grape Amount: " << m_grape_amount << endl;
 }
